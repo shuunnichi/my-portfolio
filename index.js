5440d0dea53bf12882a6c0078b6815aed3701889
shunichi <https://github.com/shuunnichi/po-toforio.git>
2024-11-12 14:31:09 +0900

    tanmati

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ let scrollPosition = 0;
 window.addEventListener('scroll', function() {
   // 現在のスクロール位置を取得
   scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  // ユーザーの画面の高さを取得
+  const windowHeight = window.innerHeight;
+  console.log('Window Height:', windowHeight);
 
   // 固定表示対象の要素が存在する場合のみ処理を行う
   if (fixedElement) {
     // 一定のスクロール量を超えたら固定表示
-    if (scrollPosition > 100) {
+    if (scrollPosition > windowHeight-900) {
       fixedElement.style.position = 'sticky';
       fixedElement.style.top = '0';
     } else {
